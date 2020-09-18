90139e5c072d4a9ee1493057fed68b3040b19966
timotheemcdonald <timotheemcdonald@gmail.com>
2020-09-18 12:01:32 -0400

    api works

---
+++ api/server.spec.js
@@ -0,0 +1,43 @@
+const supertest = require('supertest')
+
+const server = require('./server')
+const db = require('../database/dbConfig')
+
+describe("server", () => {
+
+    describe('/register', () => {
+        beforeEach( async () => {
+            await db('users').truncate()
+        })
+
+        it('should return 201 when passed correct data', () => {
+            return supertest(server)
+            .post('/api/auth/register')
+            .send({ username:'Ariadne', password: 'opera'})
+            .then(res => {
+                expect(res.status).toBe(201);
+            })
+        })
+
+            it('should fail if no password entered on register', () => {
+                return supertest(server)
+                .post('/api/auth/register')
+                .send({})
+                .then(res => {
+                    expect(res.status).toBe(400)
+                })
+            })
+
+        })
+
+    
+        // describe('/login', () => {
+        //     beforeEach(async () => {
+        //         await db('users').truncate()
+        //     })
+
+
+        // })
+
+
+})
